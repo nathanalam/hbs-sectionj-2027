066cfea518504185ee4e2266e5e909cd5922a130
Nathan Alam <nathanalam99@gmail.com>
2025-11-05 13:24:03 -0500

    Switch to Astro

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "hbs-crimson": "#a51c30",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
