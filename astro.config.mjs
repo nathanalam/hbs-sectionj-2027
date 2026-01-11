de1cc91f354bbc89de3b6c7ad0a54c714e06997c
Nathan Alam <nathanalam99@gmail.com>
2026-01-10 21:53:33 -0500

    Fix github pages routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: process.env.BASE_PATH ? "https://nathanalam.github.io" : undefined,
+  base: process.env.BASE_PATH || "/",
 });
